5449e2c7fd05cc76d1c539cc40a1d0d9c3d359cb
Raj Vardhan Singh <146967184+86-98@users.noreply.github.com>
2025-06-20 15:49:38 +0530

    Add files via upload

---
+++ next.config.ts
@@ -0,0 +1,10 @@
+import type {NextConfig} from 'next';
+
+const nextConfig: NextConfig = {
+  /* config options here */
+  images: {
+    // unoptimized: true, // Removed to enable Next.js image optimization by default
+  },
+};
+
+export default nextConfig;
